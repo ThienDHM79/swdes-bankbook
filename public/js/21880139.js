b876e91b1561116227ead2df696716eb946a2e27
Thien DAO <thien1997nus@gmail.com>
2023-05-07 16:15:48 +0700

    complete create request notice

--- public/js/21880139.js
+++ public/js/21880139.js
@@ -1,20 +1,33 @@
 'use strict';
 
 const { application } = require("express");
-    function createNotice(){
-        const bookNo = document.getElementById('temp-bankbookid').innerText().trim();
-        const amount = document.getElementById('temp-amount').innerText().trim();
-        const bookType = document.getElementById('temp-booktype').innerText().trim();
+function createNotice(){
+    const bookNo = document.getElementById('temp-bankbookid').value;
+    const amount = document.getElementById('temp-amount').value;
 
-        document.getElementById('bankbookid').innerText = bookNo;
-        document.getElementById('amount').innerText = amount;
-        document.getElementById('booktype').innerText = bookType;
-        document.getElementById('current-date').innerText = dateString;
-        // Get the current date object
-        const date = new Date();
-        
-        // Format the date as a string
-        const dateString = date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0');
-     // Display the date on the page
-        console.log("updated request to form");
-    }
+    let bookTypeEle = document.getElementById('temp-booktype');
+    let bookTypeIndex = bookTypeEle.selectedIndex;
+    let bookType = bookTypeEle.selectedOptions[0].text;
+
+    document.getElementById('bankbookid').innerText = bookNo;
+    document.getElementById('amount').innerText = amount;
+    document.getElementById('booktype').innerText = bookType;
+    
+
+    // Get the current date object
+    const date = new Date();
+    
+    // Format the date as a string
+    const dateString = date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0');
+ // Display the date on the page
+    document.getElementById('current-date').innerText = dateString;
+    console.log(`date is ${dateString}`);
+    console.log(`amount ${amount}`);
+}
+function clearRequest(){
+    document.getElementById('bankbookid').innerText = "";
+    document.getElementById('amount').innerText = "";
+    document.getElementById('booktype').innerText = "";
+    document.getElementById('current-date').innerText = "";
+}
+    
